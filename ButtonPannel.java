686d234eccec74d7b1634acb930dda12863a8c21
Akriti S Shetty <akritishetty.in@gmail.com>
2024-10-22 22:24:00 +0530

    Update ButtonPannel.java

--- ButtonPannel.java
+++ ButtonPannel.java
@@ -1,5 +1,45 @@
 package classess;
 
-public class ButtonPannel {
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
 
+import javax.swing.JButton;
+import javax.swing.JPanel;
+import javax.swing.Box;
+import javax.swing.BorderFactory;
+import javax.swing.border.Border;
+
+public class ButtonPannel extends JPanel {
+	private JButton addTask;
+	private JButton clear;
+	
+	Border emptyBorder = BorderFactory.createEmptyBorder();
+	
+	//Constructor
+	ButtonPanel () {
+		this.setPreferredSize(new Dimension(400,60));
+		
+		addTask = new JButton("Add Task");
+		addTask.setBorder(emptyBorder);
+		addTask.setFont(new Font("Sans-serif",Font.PLAIN,20));
+		
+		this.add(addTask);
+		
+		this.add(Box.createHorizontalStrut(20));
+		clear = new JButton("Clear Completed Tasks");
+		clear.setBorder(emptyBorder);
+		clear.setFont(new Font("Sans-serif",Font.PLAIN,20));
+		
+		this.add(clear);
+	}
+	
+	public JButton getAddTask() {
+		return addTask;
+	}
+	
+	public JButton getClear() {
+		return clear;
+	}
+	
 }
