a1feacc53e84e390ec2d39fbdadb9cdbeb6e7e21
Madhura-shetty <shettymadhura26@gmail.com>
2024-10-23 22:24:39 +0530

    Update list.java

--- list.java
+++ list.java
@@ -1,5 +1,23 @@
 package classess;
+import javax.swing.JPanel;
+public class list extends JPanel{
+  list(){
+    GridLayout layout = new GridLayout(10,1);
+    layout.setVgap(5);
+    
+    this.setLayout(layout);
+    //this.setBackground(Color.blue);
+ }
+  public void updateNumbers()
+  {
+    Component[] listItems = this.getComponents();
 
-public class list {
-
+    for(int i=0;i< listItems.length;i++)
+      {
+        if(listItems[i] instanceof Task)
+        {
+          ((Task)listItems[i]).changeIndex(i+1);
+        }
+      }
+  }
 }
