6fb7196661f55fdbead1c3cf255153af8f3568cb
Madhura-shetty <shettymadhura26@gmail.com>
2024-10-23 22:23:55 +0530

    Update AppFrame.java

--- AppFrame.java
+++ AppFrame.java
@@ -2,15 +2,21 @@ package classess;
 
 import java.awt.BorderLayout;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 
 public class AppFrame extends JFrame{
+	
      private titleBar title;
      private list List;
      private ButtonPannel btnPannel;
+
+	private JButton addTask;
+	private JButton clear;
 	
 
-	AppFrame(){
+	AppFrame()
+	{
     	 this.setSize(400,700);
     	 this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     	 this.setVisible(true);
@@ -20,6 +26,25 @@ public class AppFrame extends JFrame{
     	 btnPannel = new ButtonPannel();
     	 this.add(title,BorderLayout.NORTH);
     	 this.add(btnPannel, BorderLayout.SOUTH);
+
+	 this.add(list,BorderLayout.CENTER);
+		
+	 addTask = btnPanel.getAddTask();
+         clear = btnPanel.getClear();
+
+	 addListeners();	
     	 
      }
+	public void addListeners()
+	{
+		addTsk.addMouseListener(new MouseAdapter())
+		{
+			@override
+			public void mousePressed(MouseEvent e)
+			{
+				Task task = new Task();
+			        list.add(task);
+			revalidate():
+			}
+	}};
 }
