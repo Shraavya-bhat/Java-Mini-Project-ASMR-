608967b622fc1cf6ea9b9e857b8d062e926f2025
Shraavya-bhat <shraavyaknbhat@gmail.com>
2024-10-27 20:47:16 +0530

    Update task.java

--- task.java
+++ task.java
@@ -4,15 +4,19 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 
+import javax.swing.BorderFactory;
 import javax.swing.JButton;
+import javax.swing.JFormattedTextField;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+import javax.swing.border.Border;
+import javax.swing.plaf.basic.BasicIconFactory;
 
 public class Task extends JPanel {
   
   private JLabel index;
-  private JTextfield taskName;
+  private JTextField taskName;
   private JButton done;
 
   private boolean checked;
@@ -22,26 +26,40 @@ public class Task extends JPanel {
     this.setPreferredSize(new Dimension(40,20));
     this.setBackground(Color.red);
 
-    this.setLayout(new BorderLayout()):
+    this.setLayout(new BorderLayout());
+    checked = false;
 
-      checked = false;
-
-    index = new JLabel(**);
+    index = new JLabel();
     index.setPreferredSize(new Dimension(20,20));
     index.setHorizontalAlignment(JLabel.CENTER);
     this.add(index,BorderLayout.WEST);
 
-    taskName = new JTextfield("Your task here");
-    taskName.setBorder(BorderFactory.createEmptyBorder());
-    taskNmae.setBackground(Color.red);
+    taskName = new JFormattedTextField("Your task here");
+    taskName.setBorder((Border) BasicIconFactory.createEmptyFrameIcon());
+    taskName.setBackground(Color.red);
 
     this.add(taskName,BorderLayout.CENTER);
 
     done = new JButton("done");
     done.setPreferredSize(new Dimension(40,20));
-    done.setBorder(BorderFactory.createEmptyBorder());
+    done.setBorder((Border) BasicIconFactory.createEmptyFrameIcon());
 
     this.add(done,BorderLayout.EAST);
  }
+  public JButton getDone()
+  {
+	  return done;
+  }
+  public void changeIndex(int num)
+  {
+	  this.index.setText(num+"");
+	  this.revalidate();
+  }
+  public void changeState()
+  {
+	  this.setBackground(Color.green);
+	  taskName.setBackground(Color.green);
+	  checked = true;
+  }
 }  
   
